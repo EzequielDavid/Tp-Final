97fea1a043da52414ca58e68d99583129ff55824
MacaDoPazo <49454702+MacaDoPazo@users.noreply.github.com>
2020-11-16 21:11:49 -0300

    boton ingresar vehiculo nuevo

--- view/ListadoDeVehiculos.php
+++ view/ListadoDeVehiculos.php
@@ -7,7 +7,7 @@
 <body>
 <div class="w3-container w3-display-container" >
     <div class="w3-container w3-orange w3-opacity w3-hide-small" >
-        <h2>Vehiculos</h2>
+        <h2>Vehiculos</h2> <a href='#' class='w3-button w3-black'>Ingresar vehiculo</a> <br>
         <table class="w3-table-all w3-card-4">
             <tr>
                 <th>Matricula</th>
