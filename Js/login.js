67b4d84d22bd38e3219066ec7a34f31f2245647e
florenciaLP <f.lopez.pacini@gmail.com>
2020-12-08 16:48:08 -0300

    se completó proforma

--- Js/login.js
+++ Js/login.js
@@ -11,6 +11,15 @@ if (login) {
     });
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 
 
 
